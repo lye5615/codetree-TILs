f94f576096751e842929a4008d3d308df662515a
이예은 <162159470+lye5615@users.noreply.github.com>
2024-08-01 14:28:00 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240801/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] x1 = new int[n+1];
+        int[] x2 = new int[n+1];
+
+        int[] line = new int[201];
+
+        for(int i=1; i<=n; i++){
+            x1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+        }
+        for(int i=1; i<=n; i++){
+            x1[i] += 100;
+            x2[i] += 100;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j = x1[i]; j<x2[i]; j++){
+                line[j]++;
+            }
+        }
+
+        int r = 0;
+        for(int i=1;i<=200;i++){
+            if(line[i]>=2 && ){
+                ++r;
+            }
+        }
+        System.out.println(r-1);
+    }
+}
